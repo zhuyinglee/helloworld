15f4b1e081b15f9408b7ff44bfa3155ca7b05f93
psycheee <1337208225@qq.com>
2019-09-18 11:11:09 +0800

    test2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.err.println("Hello World!");
+		System.err.println("Hello World! test");
 	}
 
 }
